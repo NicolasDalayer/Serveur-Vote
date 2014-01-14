68f8adef90df9d63c2a247f3ab8981508d8084fe
AlexandreOrtiz <alexandre.ortiz.o@gmail.com>
2014-01-14 11:11:17 +0100

    Config.php

--- appvote/config.php
+++ appvote/config.php
@@ -4,7 +4,7 @@ return array
 (
 	'dsn' => 'mysql:dbname=db;host=127.0.0.2',
 	'username' => 'root',
-	'password' => '',
+	'password' => '5+4(7{EjiSeR!',
 	'options' => array
 	(
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
